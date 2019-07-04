5157db2786fa6be5664dd10ebddfd713bd86d21c
Espen Gjøstøl <espen.oskar.gjostol@nav.no>
2019-07-04 14:33:07 +0200

    PEON-861: Fix vulnerable Tomcat dependency.

--- src/main/java/no/nav/pensjon/innsyn/server/tomcat/TomcatStarter.java
+++ src/main/java/no/nav/pensjon/innsyn/server/tomcat/TomcatStarter.java
@@ -35,19 +35,11 @@ public class TomcatStarter {
     }
 
     private static void prepareContext(Tomcat server) {
-        StandardContext context = addWebapp(server);
+        var context = (StandardContext) server.addWebapp("", absolutePath(WEBAPP_RELATIVE_PATH));
         var webResourceRoot = new StandardRoot(context);
         String resourceBase = absolutePath(RELATIVE_RESOURCE_BASE);
         webResourceRoot.addPreResources(new DirResourceSet(webResourceRoot, WEBAPP_MOUNT, resourceBase, "/"));
         context.setResources(webResourceRoot);
-    }
-
-    private static StandardContext addWebapp(Tomcat server) {
-        try {
-            return (StandardContext) server.addWebapp("", absolutePath(WEBAPP_RELATIVE_PATH));
-        } catch (ServletException e) {
-            throw new AppServerException("Failed to add webapp", e);
-        }
     }
 
     private static void start(Tomcat server) {

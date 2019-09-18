fea36804aa5b6f26c92eaa62b0a3dbb0f86450b3
Mathias Sand Jahren <Mathias.Sand.Jahren@nav.no>
2019-09-18 09:10:33 +0200

    Add /isAlive

--- src/main/java/no/nav/pensjon/innsyn/server/servlet/IsReadyServlet.java
+++ src/main/java/no/nav/pensjon/innsyn/server/servlet/IsReadyServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "IsReadyServlet", urlPatterns = {"/isReady"})
+@WebServlet(name = "IsReadyServlet", urlPatterns = {"/isReady", "/isAlive"})
 public class IsReadyServlet extends HttpServlet {
 
     @Override

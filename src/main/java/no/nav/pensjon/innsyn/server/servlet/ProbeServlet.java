c1d1f66f5b1b717b3999efdd9b5d9f8b07595c10
Mathias Sand Jahren <Mathias.Sand.Jahren@nav.no>
2019-09-18 09:31:35 +0200

    Add 200 response on probes

--- src/main/java/no/nav/pensjon/innsyn/server/servlet/ProbeServlet.java
+++ src/main/java/no/nav/pensjon/innsyn/server/servlet/ProbeServlet.java
@@ -7,13 +7,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "IsReadyServlet", urlPatterns = {"/isReady", "/isAlive"})
-public class IsReadyServlet extends HttpServlet {
+@WebServlet(name = "ProbeServlet", urlPatterns = {"/isReady", "/isAlive"})
+public class ProbeServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setStatus(200);
         try (ServletOutputStream out = response.getOutputStream()) {
-            out.write("true".getBytes());
+            out.print(true);
             out.flush();
         }
     }

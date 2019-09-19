539d73795ac683e688b7ce5863b673056dd213dc
Mathias Sand Jahren <Mathias.Sand.Jahren@nav.no>
2019-09-19 14:19:57 +0200

    Print CLIENT_ID

--- src/main/java/no/nav/pensjon/innsyn/main/Application.java
+++ src/main/java/no/nav/pensjon/innsyn/main/Application.java
@@ -9,6 +9,7 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 public class Application {
 
     public static void main(String[] args) {
+        System.out.println("CLIENT_ID = " + System.getenv("CLIENT_ID"));
         SpringApplication.run(Application.class, args);
     }
 }

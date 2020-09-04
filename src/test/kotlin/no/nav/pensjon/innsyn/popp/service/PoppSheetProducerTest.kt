6fee6f47c55df60c920cbc14727aa077ec9157de
Mathias Sand Jahren <the.mather1@gmail.com>
2020-09-04 11:14:31 +0200

    Improve tests

--- src/test/kotlin/no/nav/pensjon/innsyn/popp/service/PoppSheetProducerTest.kt
+++ src/test/kotlin/no/nav/pensjon/innsyn/popp/service/PoppSheetProducerTest.kt
@@ -2,18 +2,20 @@ package no.nav.pensjon.innsyn.popp.service
 
 import com.ninjasquad.springmockk.MockkBean
 import io.mockk.every
+import no.nav.pensjon.innsyn.popp.assertEqualsTestData
 import no.nav.pensjon.innsyn.popp.domain.PoppObjects
 import no.nav.pensjon.innsyn.popp.domain.container.*
 import no.nav.pensjon.innsyn.popp.repository.*
 import org.apache.poi.ss.usermodel.CellType
 import org.apache.poi.xssf.usermodel.XSSFWorkbook
-import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 import java.io.File
 import java.io.FileInputStream
+import kotlin.test.assertEquals
+import kotlin.test.fail
 
 @SpringBootTest(classes = [PoppSheetProducer::class, BeholdningContainer::class, DagpengerContainer::class,
     ForstegangstjenesteContainer::class, FppAfpContainer::class, InntektContainer::class, OmsorgContainer::class,
@@ -49,29 +51,6 @@ internal class PoppSheetProducerTest {
             every { omsorgRepository.findAllByPersonId(1) } returns omsorg
             every { opptjeningRepository.findAllByPersonId(1) } returns opptjening
         }
-        val testBook = XSSFWorkbook(FileInputStream(File("popp-test-worksheet.xlsx")))
-        poppSheetProducer.produceWorksheet(1).apply {
-            assertEquals(testBook.numberOfSheets, numberOfSheets)
-            this.forEachIndexed { si, sheet ->
-                val testSheet = testBook.getSheetAt(si)
-                assertEquals(testSheet.physicalNumberOfRows, sheet.physicalNumberOfRows)
-                sheet.forEachIndexed { ri, row ->
-                    val testRow = testSheet.getRow(ri)
-                    assertEquals(testRow.physicalNumberOfCells, row.physicalNumberOfCells)
-                    row.forEachIndexed { ci, cell ->
-                        when(cell.cellType){
-                            CellType.NUMERIC -> assertEquals(
-                                    testRow.getCell(ci)?.numericCellValue,
-                                    cell.numericCellValue
-                            )
-                            CellType.STRING -> assertEquals(
-                                    testRow.getCell(ci)?.stringCellValue,
-                                    cell.stringCellValue
-                            )
-                        }
-                    }
-                }
-            }
-        }
+        poppSheetProducer.produceWorksheet(1).assertEqualsTestData()
     }
 }

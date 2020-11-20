b61a3a857558dbd792f1c413d29014625f50d58b
Mathias Sand Jahren <Mathias.Sand.Jahren@nav.no>
2020-11-20 09:50:07 +0100

    List database secrets to debug missing prod config.

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
+ls /secrets/oracle/config
+ls /secrets/oracle/popp
 export SPRING_DATASOURCE_URL=$(cat /secrets/oracle/config/jdbc_url)
 export SPRING_DATASOURCE_USERNAME=$(cat /secrets/oracle/popp/username)
 export SPRING_DATASOURCE_PASSWORD=$(cat /secrets/oracle/popp/password)
